277489ae896c061835981b07114c35717cda56d8
Ivan Skoric <ivanskoric7@gmail.com>
2025-08-05 23:16:39 +0200

    LC optimizations

--- leetcode/fruits-into-baskets-ii.kts
+++ leetcode/fruits-into-baskets-ii.kts
@@ -1,17 +1,21 @@
 class Solution {
     fun numOfUnplacedFruits(fruits: IntArray, baskets: IntArray): Int {
         var unplacedFruitsCount = 0
-        var fruitsFit = false
+        var fruitsFit: Boolean
+        var basketCap: Int
         for (i in fruits) {
-            for (j in baskets.indices) {
-                if (baskets[j] != 0 && baskets[j] >= i) {
+            fruitsFit = false
+            for (j in 0 until baskets.size) {
+                basketCap = baskets[j]
+                if (basketCap == 0) continue
+                if (basketCap >= i) {
                     baskets[j] = 0
                     fruitsFit = true
                     break
                 }
             }
-            if (!fruitsFit) unplacedFruitsCount++
-            fruitsFit = false
+            if (fruitsFit) continue
+            unplacedFruitsCount++
         }
         return unplacedFruitsCount
     }
