ac224f54501085c805628591d0fc5a3c3846798d
Ivan Skoric <ivanskoric7@gmail.com>
2025-08-12 00:20:47 +0200

    Update dotfiles

--- dotfiles/nvim/lua/config/init.lua
+++ dotfiles/nvim/lua/config/init.lua
@@ -1,3 +1,4 @@
 require("config.lazy")
 require("config.remap")
 require("config.settings")
+require("config.treesitter")
--- dotfiles/nvim/lua/config/remap.lua
+++ dotfiles/nvim/lua/config/remap.lua
@@ -10,10 +10,10 @@ end, {desc = "Reformat file"})
 
 -- Telescope
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>g', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>e', builtin.oldfiles, { desc = 'Telescope recently opened' })
 
 -- Buffers
 vim.keymap.set("n", "<leader>W", function()
