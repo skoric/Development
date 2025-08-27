e9af5a58eb8055678a08bcf8842f8790f802458b
Ivan Skoric <ivanskoric7@gmail.com>
2025-08-27 18:29:44 +0200

    Update dotfiles

--- dotfiles/nvim/lua/config/remap.lua
+++ dotfiles/nvim/lua/config/remap.lua
@@ -27,15 +27,3 @@ end, { desc = "Save all buffers" })
 -- Disable arrows
 local opts = { noremap = true, silent = true }
 local keymap = vim.keymap.set
-keymap('n', '<Up>', '<Nop>', opts)
-keymap('n', '<Down>', '<Nop>', opts)
-keymap('n', '<Left>', '<Nop>', opts)
-keymap('n', '<Right>', '<Nop>', opts)
-keymap('i', '<Up>', '<Nop>', opts)
-keymap('i', '<Down>', '<Nop>', opts)
-keymap('i', '<Left>', '<Nop>', opts)
-keymap('i', '<Right>', '<Nop>', opts)
-keymap('v', '<Up>', '<Nop>', opts)
-keymap('v', '<Down>', '<Nop>', opts)
-keymap('v', '<Left>', '<Nop>', opts)
-keymap('v', '<Right>', '<Nop>', opts)
